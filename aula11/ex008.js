4230e17e3efcbec19ccea7d9131573e2c0379d06
Walter Manoel <waltermanoel.pro@gmail.com>
2022-02-06 12:35:00 -0300

    Trabalhei com condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78.2
+
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+
+if (vel > 60) { // Condição simples
+    console.log(`Você utrapassou a velicidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija sempre usando sinto de segurança`)
